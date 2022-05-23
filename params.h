cb7ee37a54f1ef22e565745dd4ead61f61289d8d
Aylarerf <87969046+Aylarerf@users.noreply.github.com>
2022-05-23 11:26:32 -0700

    Update params.h

--- params.h
+++ params.h
@@ -1,16 +1,5 @@
-#ifndef XMSS_PARAMS_H
-#define XMSS_PARAMS_H
 
 #include <stdint.h>
-
-/* These are merely internal identifiers for the supported hash functions. */
-#define XMSS_SHA2 0
-#define XMSS_SHAKE128 1
-#define XMSS_SHAKE256 2
-
-/* This is a result of the OID definitions in the draft; needed for parsing. */
-#define XMSS_OID_LEN 4
-
 
 typedef struct {
     unsigned int func;
@@ -23,10 +12,6 @@ typedef struct {
     unsigned int wots_len;
     unsigned int wots_sig_bytes;
     unsigned int index_bytes;
-    unsigned int sig_bytes;
-    unsigned int pk_bytes;
-    unsigned long long sk_bytes;
-    unsigned int bds_k;
 } wots_params;
 
 
