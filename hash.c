fc8e65ef2c53bef7e099024744a67e28c31e6ce5
Aylarerf <87969046+Aylarerf@users.noreply.github.com>
2022-06-23 14:29:23 -0700

    Update hash.c

--- hash.c
+++ hash.c
@@ -8,13 +8,29 @@
 #include "hash.h"
 #include "fips202.h"
 
-#define XMSS_HASH_PADDING_F 0
-#define XMSS_HASH_PADDING_H 1
-#define XMSS_HASH_PADDING_HASH 2
-#define XMSS_HASH_PADDING_PRF 3
-#define XMSS_HASH_PADDING_PRF_KEYGEN 4
 
-void addr_to_bytes(unsigned char *bytes, const uint32_t addr[8])
+
+
+void set_key_and_mask(word32 addr[8], word32 key_and_mask)
+{
+    addr[7] = key_and_mask;
+}
+
+void set_chain_addr(word32 addr[8], word32 chain)
+{
+    addr[5] = chain;
+}
+
+void set_hash_addr(word32 addr[8], word32 hash)
+{
+    addr[6] = hash;
+}
+
+#define XMSS_HASH_PADDING_F 0
+#define XMSS_HASH_PADDING_PRF 3
+
+
+void addr_to_bytes(byte *bytes, const word32 addr[8])
 {
     int i;
     for (i = 0; i < 8; i++) {
@@ -22,154 +38,46 @@ void addr_to_bytes(unsigned char *bytes, const uint32_t addr[8])
     }
 }
 
-static int core_hash(const wots_params *params,
-                     unsigned char *out,
-                     const unsigned char *in, unsigned long long inlen)
+/*
+ * Computes PRF(key, in), for a key of PARAMSN bytes, and a 32-byte input.
+ */
+int prf(byte *out, const byte in[32],
+        const byte *key)
 {
-    unsigned char buf[64];
+    byte buf[2 * PARAMSN + 32];
 
-    if (params->n == 24 && params->func == XMSS_SHA2) {
-        SHA256(in, inlen, buf);
-        memcpy(out, buf, 24);
-    }
-    else if (params->n == 24 && params->func == XMSS_SHAKE256) {
-        shake256(out, 24, in, inlen);
-    }   
-    else if (params->n == 32 && params->func == XMSS_SHA2) {
-        SHA256(in, inlen, out);
-    }
-    else if (params->n == 32 && params->func == XMSS_SHAKE128) {
-        shake128(out, 32, in, inlen);
-    }
-    else if (params->n == 32 && params->func == XMSS_SHAKE256) {
-        shake256(out, 32, in, inlen);
-    }
-    else if (params->n == 64 && params->func == XMSS_SHA2) {
-        SHA512(in, inlen, out);
-    }
-    else if (params->n == 64 && params->func == XMSS_SHAKE256) {
-        shake256(out, 64, in, inlen);
-    }
-    else {
-        return -1;
-    }
+    ull_to_bytes(buf, PARAMSN, XMSS_HASH_PADDING_PRF);
+    memcpy(buf + PARAMSN, key, PARAMSN);
+    memcpy(buf + (2*PARAMSN), in, 32);
+    core_hash(out, buf, (2*PARAMSN) + 32);
     return 0;
 }
 
-/*
- * Computes PRF(key, in), for a key of params->n bytes, and a 32-byte input.
- */
-int prf(const wots_params *params,
-        unsigned char *out, const unsigned char in[32],
-        const unsigned char *key)
+
+int thash_f(byte *out, const byte *in,
+            const byte *pub_seed, word32 addr[8])
 {
-    unsigned char buf[params->padding_len + params->n + 32];
-
-    ull_to_bytes(buf, params->padding_len, XMSS_HASH_PADDING_PRF);
-    memcpy(buf + params->padding_len, key, params->n);
-    memcpy(buf + params->padding_len + params->n, in, 32);
-
-    return core_hash(params, out, buf, params->padding_len + params->n + 32);
-}
-
-/*
- * Computes PRF_keygen(key, in), for a key of params->n bytes, and an input
- * of 32 + params->n bytes
- */
-int prf_keygen(const wots_params *params,
-        unsigned char *out, const unsigned char *in,
-        const unsigned char *key)
-{
-    unsigned char buf[params->padding_len + 2*params->n + 32];
-
-    ull_to_bytes(buf, params->padding_len, XMSS_HASH_PADDING_PRF_KEYGEN);
-    memcpy(buf + params->padding_len, key, params->n);
-    memcpy(buf + params->padding_len + params->n, in, params->n + 32);
-
-    return core_hash(params, out, buf, params->padding_len + 2*params->n + 32);
-}
-
-/*
- * Computes the message hash using R, the public root, the index of the leaf
- * node, and the message. Notably, it requires m_with_prefix to have 3*n plus
- * the length of the padding as free space available before the message,
- * to use for the prefix. This is necessary to prevent having to move the
- * message around (and thus allocate memory for it).
- */
-int hash_message(const wots_params *params, unsigned char *out,
-                 const unsigned char *R, const unsigned char *root,
-                 unsigned long long idx,
-                 unsigned char *m_with_prefix, unsigned long long mlen)
-{
-    /* We're creating a hash using input of the form:
-       toByte(X, 32) || R || root || index || M */
-    ull_to_bytes(m_with_prefix, params->padding_len, XMSS_HASH_PADDING_HASH);
-    memcpy(m_with_prefix + params->padding_len, R, params->n);
-    memcpy(m_with_prefix + params->padding_len + params->n, root, params->n);
-    ull_to_bytes(m_with_prefix + params->padding_len + 2*params->n, params->n, idx);
-
-    return core_hash(params, out, m_with_prefix, mlen + params->padding_len + 3*params->n);
-}
-
-/**
- * We assume the left half is in in[0]...in[n-1]
- */
-int thash_h(const wots_params *params,
-            unsigned char *out, const unsigned char *in,
-            const unsigned char *pub_seed, uint32_t addr[8])
-{
-    unsigned char buf[params->padding_len + 3 * params->n];
-    unsigned char bitmask[2 * params->n];
-    unsigned char addr_as_bytes[32];
+    byte buf[3 * PARAMSN];
+    byte bitmask[PARAMSN];
+    byte addr_as_bytes[32];
     unsigned int i;
 
     /* Set the function padding. */
-    ull_to_bytes(buf, params->padding_len, XMSS_HASH_PADDING_H);
+    ull_to_bytes(buf, PARAMSN, XMSS_HASH_PADDING_F);
 
     /* Generate the n-byte key. */
     set_key_and_mask(addr, 0);
     addr_to_bytes(addr_as_bytes, addr);
-    prf(params, buf + params->padding_len, addr_as_bytes, pub_seed);
-
-    /* Generate the 2n-byte mask. */
-    set_key_and_mask(addr, 1);
-    addr_to_bytes(addr_as_bytes, addr);
-    prf(params, bitmask, addr_as_bytes, pub_seed);
-
-    set_key_and_mask(addr, 2);
-    addr_to_bytes(addr_as_bytes, addr);
-    prf(params, bitmask + params->n, addr_as_bytes, pub_seed);
-
-    for (i = 0; i < 2 * params->n; i++) {
-        buf[params->padding_len + params->n + i] = in[i] ^ bitmask[i];
-    }
-    return core_hash(params, out, buf, params->padding_len + 3 * params->n);
-}
-
-int thash_f(const wots_params *params,
-            unsigned char *out, const unsigned char *in,
-            const unsigned char *pub_seed, uint32_t addr[8])
-{
-    unsigned char buf[params->padding_len + 2 * params->n];
-    unsigned char bitmask[params->n];
-    unsigned char addr_as_bytes[32];
-    unsigned int i;
-
-    /* Set the function padding. */
-    ull_to_bytes(buf, params->padding_len, XMSS_HASH_PADDING_F);
-
-    /* Generate the n-byte key. */
-    set_key_and_mask(addr, 0);
-    addr_to_bytes(addr_as_bytes, addr);
-    prf(params, buf + params->padding_len, addr_as_bytes, pub_seed);
+    prf(buf + PARAMSN, addr_as_bytes, pub_seed);
 
     /* Generate the n-byte mask. */
     set_key_and_mask(addr, 1);
     addr_to_bytes(addr_as_bytes, addr);
-    prf(params, bitmask, addr_as_bytes, pub_seed);
+    prf(bitmask, addr_as_bytes, pub_seed);
 
-    for (i = 0; i < params->n; i++) {
-        buf[params->padding_len + params->n + i] = in[i] ^ bitmask[i];
+    for (i = 0; i < PARAMSN; i++) {
+        buf[2*PARAMSN + i] = in[i] ^ bitmask[i];
     }
-    return core_hash(params, out, buf, params->padding_len + 2 * params->n);
+    core_hash(out, buf, 3 * PARAMSN);
+    return 0;
 }
