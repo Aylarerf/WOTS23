6bd051404b5e20a931adfedbbeb75482cc32b894
Aylarerf <87969046+Aylarerf@users.noreply.github.com>
2022-06-24 13:03:40 -0700

    Update params.h

--- params.h
+++ params.h
@@ -1,5 +1,7 @@
 
 #include <stdint.h>
+#include <string.h>
+#define n 32
 
 typedef struct {
    
@@ -17,6 +19,16 @@ typedef struct {
 
 
     
-int wots_initialize_params(wots_params *params);
+int wots_initialize_params(wots_params *params){
+   if (params->wots_w == 16) {
+        params->wots_log_w = 4;
+        params->wots_len1 = 8 * params->n / params->wots_log_w;
+        /* len_2 = floor(log(len_1 * (w - 1)) / log(w)) + 1 */
+        params->wots_len2 = 3;
+    }
+  
+    params->wots_len = params->wots_len1 + params->wots_len2;
+    params->wots_sig_bytes = params->wots_len * params->n;
+}
 
 
